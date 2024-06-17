da9df931a6078dedec0c89f26a6bd1558f7453e9
Andjelko28 <105885932+Andjelko28@users.noreply.github.com>
2024-06-17 14:09:39 +0200

    first commit

---
+++ resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,21 @@
+<x-panel class="flex gap-x-6">
+    <div>
+        <x-employer-logo></x-employer-logo>
+    </div>
+
+    <div class="flex-1 flex flex-col">
+        <a href="#" class="self-start text-sm text-gray-400">Laracasts</a>
+
+        <div class="py-1 font-bold">
+            <h3 class="font-bold text-lg mt-3 group-hover:text-blue-600 text-xl font-bol">Video Producer</h3>
+            <p class="text-sm text-gray-400 mt-4">Full Time - From $60,00</p>
+        </div>
+    </div>
+
+    <div>
+        <x-tag>Tag</x-tag>
+        <x-tag>Tag</x-tag>
+        <x-tag>Tag</x-tag>
+
+    </div>
+</x-panel>
