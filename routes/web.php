dc2581fa3d39d8089429c0ed5de58431d0debd4c
Andjelko28 <105885932+Andjelko28@users.noreply.github.com>
2024-06-18 11:01:33 +0200

    "Update JobController, jobs index view, and web routes"

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\JobController;
+use \App\Http\Controllers\RegisteredUserController;
+use \App\Http\Controllers\SessionController;
 
 Route::get('/', [JobController::class, 'index']);
+
+Route::get('/register', [RegisteredUserController::class], 'create');
+Route::get('/register', [RegisteredUserController::class], 'store');
+
+Route::get('/login', [SessionController::class], 'create');
+Route::get('/login', [SessionController::class], 'store');
+Route::delete('/login', [SessionController::class], 'destroy');
