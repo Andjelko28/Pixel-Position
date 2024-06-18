c4d7415a887a7243becb042e0638ae439350a109
Andjelko28 <105885932+Andjelko28@users.noreply.github.com>
2024-06-18 11:48:34 +0200

    "Updated RegisteredUserController, added validation rules, installed laravel-debugbar package, updated composer files, modified app.php, register.blade.php, and web.php files"

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,16 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+
 use \App\Http\Controllers\JobController;
 use \App\Http\Controllers\RegisteredUserController;
 use \App\Http\Controllers\SessionController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', [JobController::class, 'index']);
 
-Route::get('/register', [RegisteredUserController::class], 'create');
-Route::get('/register', [RegisteredUserController::class], 'store');
+Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
 
-Route::get('/login', [SessionController::class], 'create');
-Route::get('/login', [SessionController::class], 'store');
-Route::delete('/login', [SessionController::class], 'destroy');
+Route::get('/login', [SessionController::class, 'create']);
+Route::get('/login', [SessionController::class, 'store']);
+Route::delete('/logout', [SessionController::class, 'destroy']);
